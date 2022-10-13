1db8b4436e823dd016974b29dd274128310a3a1b
Matin Noohnezhad <matin.noohnezhad@gmail.com>
2022-10-13 19:27:08 +0330

    Simplified two lines | improve_design

--- actions/wolf_sheep_grass/eight_way_random_walk.py
+++ actions/wolf_sheep_grass/eight_way_random_walk.py
@@ -7,8 +7,8 @@ class EightWayRandomWalk(Action):
 
     def act(self, agent, env):
         # 1 or 0 or -1
-        x_move = round(random.random() * 2) - 1
+        x_move = random.randint(-1, 1)
         # 1 or 0 or -1
-        y_move = round(random.random() * 2) - 1
+        y_move = random.randint(-1, 1)
         #
         walk(agent, env, x_move, y_move)
