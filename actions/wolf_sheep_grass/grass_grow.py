2577754e9cc8aacd6ea50b7d8b0b32636d35164a
Matin Noohnezhad <matin.noohnezhad@gmail.com>
2022-10-14 11:41:44 +0330

    Cleaning a code. Also find a bug and fixed it. | improve_design

--- actions/wolf_sheep_grass/grass_grow.py
+++ actions/wolf_sheep_grass/grass_grow.py
@@ -1,6 +1,3 @@
-import copy
-import random
-
 from actions.action import Action
 
 
@@ -9,6 +6,6 @@ class GrassGrow(Action):
     def act(self, grass, env):
         if not grass.green:
             grass.time_to_grow -= 1
-            if grass.time_to_grow == 0:
+            if grass.time_to_grow <= 0:
                 grass.green = True
                 grass.time_to_grow = grass.regrowth_time
